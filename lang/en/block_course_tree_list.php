59edfd2766c5133d56bb842891ab755c4aa52803
Jay Huber <jhuber@colum.edu>
2012-12-06 09:25:32 -0600

    Moodle 2.4 Enhancements

--- lang/en/block_course_tree_list.php
+++ lang/en/block_course_tree_list.php
@@ -30,8 +30,8 @@ $string['pluginname_help'] = 'Course Tree List';
 $string['pluginname_link'] = 'block/course_tree_list';
 $string['pluginnamesummary'] = 'Display only user enrolled courses in heirarchy collapsible menu';
 $string['plugindisplayname'] = 'Course List';
-
-
+$string['course_tree_list:addinstance'] = 'Add a new course tree block';
+$string['course_tree_list:myaddinstance'] = 'Add a new course tree block to the My Moodle page';
 
 $string['weeksbefore'] = 'Weeks Before';
 $string['descweeksbefore'] = 'If a category contains a class, how many weeks before the class to default the category open.';
